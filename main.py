65d14283c31f696a4d92f174db585c79bf6ec14f
Jnolley <jackmnolley@gmail.com>
2023-05-08 16:13:18 -0500

    added polynomial option

--- main.py
+++ main.py
@@ -3,70 +3,71 @@ import numpy as np
 import datetime
 from dateutil.relativedelta import relativedelta
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 
-def predict_future_price(past_prices, future_time_steps):
-    # Convert the past prices to a 2D array
-    past_prices = np.array(past_prices).reshape(-1, 1)
-
-    # Create a linear regression model and fit it to the past prices
-    model = LinearRegression()
-    model.fit(np.arange(len(past_prices)).reshape(-1, 1), past_prices)
-
-    # Use the model to predict future prices
-    future_prices = model.predict(np.arange(len(past_prices), len(past_prices) + future_time_steps).reshape(-1, 1))
-
-    return future_prices.flatten()
-
-def predict_future_price_test(X_train, y_train, X_test):
+def predict_future_price_linear(X_train, y_train, X_test):
     model = LinearRegression()
     model.fit(X_train, y_train)
-
     future_prices = model.predict(X_test)
-
     return future_prices
 
+
+def predict_future_price_poly(X_train, y_train, X_test, degree=2):
+    model = make_pipeline(PolynomialFeatures(degree), LinearRegression())
+    model.fit(X_train, y_train)
+    future_prices = model.predict(X_test)
+    return future_prices
+
+
 def main():
-    ticker = input("Enter the ticker symbol you want to predict the future price of: ")
-    years = input("How many years out do you want to predict? ")
-    data_range = input("How many years of historical data do you want to use for the calculation? ")
+    while True:
+        print("Select a method for stock price prediction:")
+        print("1. Linear Regression")
+        print("2. Polynomial Regression")
+        print("3. Exit")
+        choice = input("Enter the number corresponding to your choice: ")
 
-    start_date = (datetime.datetime.today() - relativedelta(years=int(data_range))).strftime('%Y-%m-%d')
-    end_date = datetime.datetime.today().strftime('%Y-%m-%d')
+        if choice == "3":
+            break
 
-    data = yf.download(ticker, start=start_date, end=end_date)
-    monthly_close_prices = data.resample('M').last()['Close']
+        ticker = input("Enter the ticker symbol you want to predict the future price of: ")
+        years = input("How many years out do you want to predict? ")
+        data_range = input("How many years of historical data do you want to use for the calculation? ")
 
-    X = np.arange(len(monthly_close_prices)).reshape(-1, 1)
-    y = np.array(monthly_close_prices).reshape(-1, 1)
+        start_date = (datetime.datetime.today() - relativedelta(years=int(data_range))).strftime('%Y-%m-%d')
+        end_date = datetime.datetime.today().strftime('%Y-%m-%d')
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        data = yf.download(ticker, start=start_date, end=end_date)
+        monthly_close_prices = data.resample('M').last()['Close']
 
-    # future price calculations using split data
-    future_prices = predict_future_price_test(X_train, y_train, X_test)
+        X = np.arange(len(monthly_close_prices)).reshape(-1, 1)
+        y = np.array(monthly_close_prices).reshape(-1, 1)
 
-    mse = mean_squared_error(y_test, future_prices)
-    r2 = r2_score(y_test, future_prices)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    print(f"Mean Squared Error: {mse:.2f}")
-    print(f"R-squared: {r2:.2f}")
+        if choice == "1":
+            prediction_method = predict_future_price_linear
+        elif choice == "2":
+            degree = int(input("Enter the degree of the polynomial (e.g., 2 for quadratic, 3 for cubic): "))
+            prediction_method = lambda X_train, y_train, X_test: predict_future_price_poly(X_train, y_train, X_test, degree)
 
-    # future price calculation using all data
-    future_price = predict_future_price(monthly_close_prices, int(years) * 12)[-1]
+        future_prices = prediction_method(X_train, y_train, X_test)
 
-    print(f"Extrapolation via Linear Regression predicts that {ticker} will be worth ${future_price:.2f} in {years} years based on {data_range} years of historical data.")
+        mse = mean_squared_error(y_test, future_prices)
+        r2 = r2_score(y_test, future_prices)
 
-     # plot actual data vs predicted data
-    plt.scatter(X_test, y_test, color='black', label='Actual Data')
-    plt.plot(X_test, future_prices, color='blue', linewidth=3, label='Predicted Data')
-    plt.title(f"{ticker} Stock Price Prediction")
-    plt.xlabel('Months')
-    plt.ylabel('Price')
-    plt.legend()
-    plt.show()
+        print(f"Mean Squared Error: {mse:.2f}")
+        print(f"R-squared: {r2:.2f}")
+
+        future_price = future_prices[-1]
+
+        print(f"Extrapolation via {'Linear' if choice == '1' else 'Poly'} Regression predicts that {ticker} will be worth ${future_price.tolist()[0]:.2f}")
+
 
 if __name__ == '__main__':
     main()
